c9a520b86bb87a17f9f60d5c3a2cd30140fbde29
step_65 <0345772@ukr.net>
2022-05-11 14:16:13 +0300

    Finish

--- controllers/api-post-controller.js
+++ controllers/api-post-controller.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 
 const handleError = (res, error) => {
-  res.status(500).send(error)
+  res.status(500).send(error.message)
   
 }
 
@@ -36,7 +36,7 @@ const editPost = (req, res) => {
   const { title, author, text } = req.body;
   const { id } = req.params;
   Post
-    .findByIdAndUpdate(id, { title, author, text })
+    .findByIdAndUpdate(id, { title, author, text }, {new:true })
     .then((post) => res.status(200).json(post))
     .catch((error) => handleError(res, error))
 };
